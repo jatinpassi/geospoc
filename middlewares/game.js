83b32e4b530c9ccbdc90c36dbab1d9b1c2e49336
jatinpassi <jatinpassi111@gmail.com>
2022-08-05 10:29:47 +0530

    word db

--- middlewares/game.js
+++ middlewares/game.js
@@ -1,41 +1,46 @@
-module.exports.gameLogic = async function (req, res, next) {
-    let comparableWord = "fiver";
-    let respArr = req.body.word.split("").map((char, index) => {
-        if (comparableWord.indexOf(char) === index) {
-            return "correct";
-        } else if (comparableWord.indexOf(char) !== -1) {
-            let regex = new RegExp(char, "g"),
-                result,
-                comparableCharArr = [],
-                wordCharArr = [];
-            while ((result = regex.exec(comparableWord))) {
-                comparableCharArr.push(result.index);
-            }
-            while ((result = regex.exec(req.body.word))) {
-                wordCharArr.push(result.index);
-            }
+let db = require("../db/word");
 
-            if (comparableCharArr.length >= wordCharArr.length) {
-                return "improve";
-            } else {
-                if (
-                    wordCharArr
-                        .slice(
-                            0,
-                            comparableCharArr.filter((ele) => {
-                                return wordCharArr.indexOf(ele) === -1;
-                            }).length
-                        )
-                        .indexOf(index) !== -1
-                ) {
+module.exports.gameLogic = async function (req, res, next) {
+    let comparableWord = db[new Date().getMonth()].toLowerCase();
+    let respArr = req.body.word
+        .toLowerCase()
+        .split("")
+        .map((char, index) => {
+            if (comparableWord.indexOf(char) === index) {
+                return "correct";
+            } else if (comparableWord.indexOf(char) !== -1) {
+                let regex = new RegExp(char, "g"),
+                    result,
+                    comparableCharArr = [],
+                    wordCharArr = [];
+                while ((result = regex.exec(comparableWord))) {
+                    comparableCharArr.push(result.index);
+                }
+                while ((result = regex.exec(req.body.word))) {
+                    wordCharArr.push(result.index);
+                }
+
+                if (comparableCharArr.length >= wordCharArr.length) {
                     return "improve";
                 } else {
-                    return "wrong";
+                    if (
+                        wordCharArr
+                            .slice(
+                                0,
+                                comparableCharArr.filter((ele) => {
+                                    return wordCharArr.indexOf(ele) === -1;
+                                }).length
+                            )
+                            .indexOf(index) !== -1
+                    ) {
+                        return "improve";
+                    } else {
+                        return "wrong";
+                    }
                 }
             }
-        }
-        return "wrong";
-    });
+            return "wrong";
+        });
     req.roundResult = respArr;
     next();
 };
--- routes/game.js
+++ routes/game.js
@@ -57,21 +57,4 @@ router.post(
     }
 );
 
-router.post("/new/words", authMiddleware.JWTChecker, async function (req, res) {
-    logger.info("[Game New Word Api] Request Received");
-    let message = "";
-    if (typeof req.body.word === "string" && req.body.word.length === 5) {
-        let word = await model.words.findOne({ word: req.body.word });
-        if (word) {
-            message = "Entry for this word exists";
-        } else {
-            await model.words.create({ word: req.body.word });
-            message = "Successfully created a new word";
-        }
-    } else {
-        message = "Please give a valid word";
-    }
-    res.send({ message });
-});
-
 module.exports = router;
